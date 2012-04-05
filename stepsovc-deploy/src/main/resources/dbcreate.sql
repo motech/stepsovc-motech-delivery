b08323ff1f323b42e790e5bc523d6774d50bddc3
Sathishkumar Thangavel <tsathish@thoughtworks.com>
2012-04-05 14:41:42 +0530

    Sathish/Balaji: Added Commcare deploy script

---
+++ stepsovc-deploy/src/main/resources/dbcreate.sql
@@ -0,0 +1,4 @@
+CREATE USER postgres WITH PASSWORD 'password';
+CREATE DATABASE commcarehq;
+GRANT ALL PRIVILEGES ON DATABASE commcarehq to postgres;
+\q
---
+++ stepsovc-deploy/src/main/resources/sitecreate.sql
@@ -0,0 +1,2 @@
+insert into django_site values (1,'localhost:8000','localhost');
+\q
