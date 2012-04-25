6acf2da8e324a4e9d1dcf58965bf369a8815f95d
Sathishkumar Thangavel <tsathish@thoughtworks.com>
2012-04-25 11:41:30 +0530

    Sathish, Dhivhya | modified view to include commcare users and domains

--- stepsovc-deploy/src/main/resources/couchdb/mirror/push.sh
+++ stepsovc-deploy/src/main/resources/couchdb/mirror/push.sh
@@ -3,14 +3,21 @@ export IFS
 cd files
 rev=""
 for doc_file_name in *.json; do
-        url="http://localhost:5984/test/${doc_file_name:0:${#doc_file_name}-5}"
+        url="http://localhost:5984/commcarehq/${doc_file_name:0:${#doc_file_name}-5}"
 	attachments=$(cat $doc_file_name | jsawk 'attachments=""; for(key in this._attachments)attachments+=key+";"; return attachments')
 	prev_rev=$(curl $url | grep { | jsawk 'return this._rev')
 	#curl -i -X DELETE $url -H "If-Match: $existing"
-	
 	modified_app=$(cat $doc_file_name | jsawk 'this._rev="'$prev_rev'"; delete this._attachments;')
-        #echo "modified app $modified_app"
-        revno=$(curl -i -X PUT $url -H "Content-Type:application/json"  -d $modified_app | grep { | jsawk 'return this.rev')
+	if [ "$prev_rev" == "" ];
+	then
+		modified_app=$(echo $modified_app | jsawk 'delete this._rev;')
+	fi
+	#echo "$doc_file_name ================> $prev_rev"
+	#echo "modified_app ___________________> $modified_app"
+
+	reply=$(curl -i -X PUT $url -H "Content-Type:application/json"  -d $modified_app)
+        #echo "reply -----------------> $reply"
+	revno=$($reply | grep { | jsawk 'return this.rev')
 	rev="?rev=$revno"
 	for attachment in $attachments; do
 		#echo $rev
