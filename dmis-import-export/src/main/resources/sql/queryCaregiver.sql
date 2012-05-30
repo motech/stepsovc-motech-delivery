f52e6af95e24a9c9cf997403786e5af1a5f6aa57
Balaji G <balajig@Balajis-MacBook-Pro.local>
2012-05-30 20:26:26 +0530

    Balaji/Geetha : | Added  Migration properties for DMIS import functionality

--- dmis-import-export/src/main/resources/sql/queryCaregiver.sql
+++
@@ -1,4 +0,0 @@
-select cg_id as caregiverId, RIGHT(cg_code,5) as caregiverCode,
-       cg_first_name as firstName, cg_middle_name as middleName, cg_last_name as lastName,
-       cg_gender as gender, cg_cell_no as phoneNumber
-	   from tbl_caregiver
---
+++ stepsovc-deploy/src/main/resources/sql/queryCaregiver.sql
@@ -0,0 +1,7 @@
+SELECT cg_id AS caregiverId, cg_code AS caregiverCode, RIGHT(cg_code,5) AS userName,
+       cg_first_name AS firstName, cg_middle_name AS middleName, cg_last_name AS lastName,
+       CASE cg_gender
+            WHEN 0 THEN 'Female'
+            WHEN 1 THEN 'Male'
+            WHEN -1 THEN 'Not Specified' END AS gender
+	   FROM tbl_caregiver
