b605f3ec1411775fc6c621689683382bc7fefc54
Balaji G <balajig@Balajis-MacBook-Pro.local>
2012-07-03 13:11:56 +0530

    Dhivhya/Balaji | Changed Nagios  Plugins

---
+++ stepsovc-deploy/src/main/resources/scripts/configure_startupservices.sh
@@ -0,0 +1,46 @@
+chkconfig  --level 0  postgresql-9.1 on
+chkconfig  --level 1  postgresql-9.1 on
+chkconfig  --level 2  postgresql-9.1 on
+chkconfig  --level 3  postgresql-9.1 on
+chkconfig  --level 4  postgresql-9.1 on
+chkconfig  --level 5  postgresql-9.1 on
+chkconfig  --level 6  postgresql-9.1 on
+
+echo "Adding tomcat  to startup"
+
+chkconfig  --level 0  tomcat on
+chkconfig  --level 1  tomcat on
+chkconfig  --level 2  tomcat on
+chkconfig  --level 3  tomcat on
+chkconfig  --level 4  tomcat on
+chkconfig  --level 5  tomcat on
+chkconfig  --level 6  tomcat on
+
+echo "Adding HTTPD  to startup"
+chkconfig  --level 0  httpd  on
+chkconfig  --level 1  httpd  on
+chkconfig  --level 2  httpd  on
+chkconfig  --level 3  httpd  on
+chkconfig  --level 4  httpd  on
+chkconfig  --level 5  httpd  on
+chkconfig  --level 6  httpd  on
+
+echo "Adding Activemq to startup"
+chkconfig  --level 0  activemq  on
+chkconfig  --level 1  activemq  on
+chkconfig  --level 2  activemq  on
+chkconfig  --level 3  activemq  on
+chkconfig  --level 4  activemq  on
+chkconfig  --level 5  activemq  on
+chkconfig  --level 6  activemq  on
+
+
+echo "Removing  CouchDB  from startup"
+
+chkconfig  --level 0  couchdb off
+chkconfig  --level 1  couchdb off
+chkconfig  --level 2  couchdb off
+chkconfig  --level 3  couchdb off
+chkconfig  --level 4  couchdb off
+chkconfig  --level 5  couchdb off
+chkconfig  --level 6  couchdb off
