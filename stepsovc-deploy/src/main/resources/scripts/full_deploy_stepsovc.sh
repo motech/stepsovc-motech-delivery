e136d8512d6b6e53d70e25311c5962142337fda9
Sathishkumar Thangavel <tsathish@thoughtworks.com>
2012-07-04 11:48:19 +0530

    Sathish/Geetha | refactored deploy scripts - fixed

--- stepsovc-deploy/src/main/resources/scripts/full_deploy_stepsovc.sh
+++ stepsovc-deploy/src/main/resources/scripts/full_deploy_stepsovc.sh
@@ -1,8 +1,9 @@
 sh drop_all_couchdb.sh
 sh stop_all.sh
 sh deploy_commcare.sh $1
-cd ~/bootstepsovc
-ant -f stepsovc_dest/deploy.xml -Denv=$1 recreatedb.and.deploy.stepsovc.from.hudson
+cd ~/bootstepsovc/stepsovc_dest
+ant -f deploy.xml -Denv=$1 recreatedb.and.deploy.stepsovc.from.hudson
+cd scripts
 sh start_all.sh
 sh post_start.sh
 sh migration_full.sh $1
