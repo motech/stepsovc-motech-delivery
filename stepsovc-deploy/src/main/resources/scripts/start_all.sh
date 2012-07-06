480684535b7d0b3c2a256e438c3c10b9b89f995c
Sathishkumar Thangavel <tsathish@thoughtworks.com>
2012-07-06 10:44:08 +0530

    Sathish | Modified to make commcare listen on 0.0.0.0 host

--- stepsovc-deploy/src/main/resources/scripts/start_all.sh
+++ stepsovc-deploy/src/main/resources/scripts/start_all.sh
@@ -1,2 +1,2 @@
-~/projects/commcare-hq/manage.py runcpserver host=192.168.42.33 port=8000 &
+~/projects/commcare-hq/manage.py runcpserver host=0.0.0.0 port=8000 &
 ~/projects/commcare-hq/manage.py celeryd -v 2 -B -s celery -E -l INFO &
