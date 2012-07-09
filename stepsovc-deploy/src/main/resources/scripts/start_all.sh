dacf2709ec08ace5d1e2f307374285eecf448fd5
Sathishkumar Thangavel <tsathish@thoughtworks.com>
2012-07-09 15:39:43 +0530

    Sathish | Modified to start the scripts from commcare directory

--- stepsovc-deploy/src/main/resources/scripts/start_all.sh
+++ stepsovc-deploy/src/main/resources/scripts/start_all.sh
@@ -1,2 +1,3 @@
-~/projects/commcare-hq/manage.py runcpserver host=0.0.0.0 port=8000 &
-~/projects/commcare-hq/manage.py celeryd -v 2 -B -s celery -E -l INFO &
+cd ~/projects/commcare-hq
+./manage.py runcpserver host=0.0.0.0 port=8000 &
+./manage.py celeryd -v 2 -B -s celery -E -l INFO &
