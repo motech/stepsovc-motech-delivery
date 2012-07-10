d14b4665f8e40e4843f436200a8f3686140ae4e6
Sathishkumar Thangavel <tsathish@thoughtworks.com>
2012-07-10 12:53:43 +0530

    Sathish | Corrected domain name for showcase

--- stepsovc-deploy/src/main/resources/sql/postdbcreate_showcase.sql
+++ stepsovc-deploy/src/main/resources/sql/postdbcreate_showcase.sql
@@ -1,3 +1,3 @@
 delete from django_site;
-insert into django_site values (1,'192.168.42.34','showcase.ovc.thoughtworks.com');
+insert into django_site values (1,'uat.ovc.thoughtworks.com','uat.ovc.thoughtworks.com');
 \q
