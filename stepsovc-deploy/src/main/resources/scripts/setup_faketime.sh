2de20b3ec2c0346355df381dee225027c952418f
Sathishkumar Thangavel <tsathish@thoughtworks.com>
2012-07-11 13:07:35 +0530

    Sathish, Balaji| Added faketime setup script

---
+++ stepsovc-deploy/src/main/resources/scripts/setup_faketime.sh
@@ -0,0 +1,11 @@
+git clone https://github.com/dileepbapat/faketime.git
+cd faketime
+gcc -shared -I $JAVA_HOME/include -I /usr/java/jdk1.7.0_05/include/linux/ -m64 jvmfaketime.c  -o libjvmfaketime.so -fPIC
+cp libjvmfaketime.so $JAVA_HOME/jre/lib/amd64/
+wget http://mirrors.ibiblio.org/pub/mirrors/maven2/javassist/javassist/3.0/javassist-3.0.jar
+mv javassist-3.0.jar javassist.jar
+javac -classpath .:javassist.jar ClassModifier.java
+java -cp .:javassist.jar  ClassModifier
+jar -uf $JAVA_HOME/jre/lib/rt.jar  java/
+javac -bootclasspath .:$JAVA_HOME/jre/lib/rt.jar -cp . Test.java
+java -cp . Test
