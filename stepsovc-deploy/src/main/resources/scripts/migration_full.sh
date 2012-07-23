3824c0b59e4f18c560baf61f5c6f4a0c38aad19c
Sathishkumar Thangavel <tsatiz@gmail.com>
2012-07-23 10:09:48 +0530

    Sathish, Dhivhya| Modified to migrate facility and caregiver phone numbers from different directory

--- stepsovc-deploy/src/main/resources/scripts/migration_full.sh
+++ stepsovc-deploy/src/main/resources/scripts/migration_full.sh
@@ -1,7 +1,7 @@
 cd ~/bootstepsovc/stepsovc_dest
-ant -f migrate.xml migrate.facility -Denv=$1
-ant -f migrate.xml migrate.caregiver -Denv=$1
-ant -f migrate.xml migrate.beneficiary -Denv=$1
-ant -f migrate.xml migrate.caregiver.phonenumber -Denv=$1
-ant -f migrate.xml migrate.facility.phonenumber -Denv=$1
-ant -f migrate.xml create.case -Denv=$1
+ant -f migrate.xml migrate.facility -Denv=$1 -Dmigration.files.root=~/bootstepsovc/stepsovc_dest/csv
+ant -f migrate.xml migrate.caregiver -Denv=$1 -Dmigration.files.root=~/bootstepsovc/stepsovc_dest/csv
+ant -f migrate.xml migrate.beneficiary -Denv=$1 -Dmigration.files.root=~/bootstepsovc/stepsovc_dest/csv
+ant -f migrate.xml migrate.caregiver.phonenumber -Denv=$1 -Dmigration.files.root=~/bootstepsovc/stepsovc_dest/csv
+ant -f migrate.xml migrate.facility.phonenumber -Denv=$1 -Dmigration.files.root=~/bootstepsovc/stepsovc_dest/csv
+ant -f migrate.xml create.case
