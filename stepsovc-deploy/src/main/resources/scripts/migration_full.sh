79388063001b44f629b899fa736a28796c602803
Sathishkumar Thangavel <tsatiz@gmail.com>
2012-07-27 11:01:05 +0530

    Sathish| corrected activemq url and migration script

--- stepsovc-deploy/src/main/resources/scripts/migration_full.sh
+++ stepsovc-deploy/src/main/resources/scripts/migration_full.sh
@@ -4,4 +4,4 @@ ant -f migrate.xml migrate.caregiver -Denv=$1 -Dmigration.files.root=~/bootsteps
 ant -f migrate.xml migrate.beneficiary -Denv=$1 -Dmigration.files.root=~/bootstepsovc/stepsovc_dest/csv
 ant -f migrate.xml migrate.caregiver.phonenumber -Denv=$1 -Dmigration.files.root=~/bootstepsovc/stepsovc_dest/csv
 ant -f migrate.xml migrate.facility.phonenumber -Denv=$1 -Dmigration.files.root=~/bootstepsovc/stepsovc_dest/csv
-ant -f migrate.xml create.case
+ant -f migrate.xml create.case -Denv=$1
